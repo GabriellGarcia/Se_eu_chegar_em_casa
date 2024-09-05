299993f0dada3113549a2a8d1741798a6faa34f5
[EXT] - GABRIEL GARCIA MORAIS <gamorais@brasilseg.com.br>
2024-09-05 12:16:08 -0300

    Primeiro Git

---
+++ Se eu chegar em casa.py
@@ -0,0 +1,12 @@
+se_eu_chegar_depois_das_19H = True
+fome = False
+não_cheguei_em_casa = False
+
+if se_eu_chegar_depois_das_19H and fome:
+    print('Vou comer e gravar vídeo depois.')
+elif se_eu_chegar_depois_das_19H and not fome:
+    print('Hoje tem liveee na roxinha')
+elif não_cheguei_em_casa:
+    print('Foi porque a merda do Trem quebrouuu :()')
+else:
+    print('Vou jogar um Vavas e fazer uma livezinha com a tropa')
